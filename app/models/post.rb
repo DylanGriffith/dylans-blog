125f4282b0d62c7a46afc0a3624a75fea2def986
Dylan Griffith <dyl.griffith@gmail.com>
2014-09-27 21:51:21 +1000

    support markdown for post body

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,13 @@
+require 'redcarpet'
+
 class Post < ActiveRecord::Base
   def self.latest_post
     order(:created_at).last
+  end
+
+  def rendered_body
+    renderer = Redcarpet::Render::HTML.new
+    markdown = Redcarpet::Markdown.new(renderer)
+    markdown.render(self.body.html_safe).html_safe
   end
 end
