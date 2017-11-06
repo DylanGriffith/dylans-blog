a13292cd392a8dceda257c0e4aa364a66d541a28
Dylan Griffith <dyl.griffith@gmail.com>
2017-11-06 21:36:53 +1100

    Random db/schema.rb update...

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20140928073919) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: true do |t|
+  create_table "posts", force: :cascade do |t|
     t.text     "title"
     t.text     "body"
     t.datetime "created_at"
